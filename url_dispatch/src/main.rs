6726b2dcbdb2723aa512496bde0fe385bc88f1e0
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-03-10 18:51:33 -0700

    updated url dispatch with main

--- url_dispatch/src/main.rs
+++ url_dispatch/src/main.rs
@@ -302,23 +302,23 @@
 //     .await
 // }
 
-// use actix_web::{guard, web, App, HttpResponse, HttpServer};
-//
-// #[actix_rt::main]
-// async fn main() -> std::io::Result<()> {
-//     HttpServer::new(|| {
-//         App::new().route(
-//             "/",
-//             web::route()
-//                 .guard(guard::Not(guard::Get()))
-//                 .to(|| HttpResponse::MethodNotAllowed())
-//         )
-//     })
-//     .bind("127.0.0.1:8088")?
-//     .run()
-//     .await
-// }
-//
+use actix_web::{guard, web, App, HttpResponse, HttpServer};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().route(
+            "/",
+            web::route()
+                .guard(guard::Not(guard::Get()))
+                .to(|| HttpResponse::MethodNotAllowed())
+        )
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
+}
+
 // > curl -v -X PUT localhost:8088/                                                                                                                            ~@thelostwayfarer.local
 // *   Trying ::1...
 // * TCP_NODELAY set
